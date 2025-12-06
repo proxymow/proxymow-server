72a35c4035dcc99dea19e42b18c7b519f22651e5
proxymow <dev@proxymow.co.uk>
2025-12-06 18:07:21 +0000

    removed test code

--- sightings_manager.py
+++ sightings_manager.py
@@ -36,3 +36,22 @@ class SightingsManager():
         
     def __repr__(self):
         return 'Num Sightings: {} {}'.format(self.count, self.sighting_clusters)
+
+if __name__ == '__main__':
+    '''
+        Class Tests
+    '''
+    threshold = 0.1 # m
+    mgr = SightingsManager(threshold)
+    p1 = (3.1, 2.1, 0)
+    p2 = (3.15, 2.15, 30)
+    p3 = (3.0, 2.0, 60)
+    p4 = (3.125, 2.125, 90)
+    p5 = (4.0, 5.0, 180)
+    p6 = (3.1, 2.1, 270)
+    poses = [p1, p2, p3, p4, p5, p6]
+    
+    for p in poses:
+        print('adding', p)
+        mgr.add(p)
+    print(mgr)
