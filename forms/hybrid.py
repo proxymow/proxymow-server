25a80e740a559c2bd1e7bf498e76567f0f34bd03
proxymow <dev@proxymow.co.uk>
2026-05-06 06:59:12 +0100

    removed verbose field comments

--- forms/hybrid.py
+++ forms/hybrid.py
@@ -6,18 +6,6 @@ class Hybrid(Term):
         represents a variable term available to the rules
         the expression can be edited, but no other fields
         the term cannot be deleted
-
-        name: str
-        description: str
-        expression: str
-        result: str
-        units: str
-        alt_expression: str
-        alt_result: str
-        alt_units: str
-        user_defined: bool
-        colour: str
-
     '''
     pk_att_name = 'name'
 
