a17d3fe7d8c641e1778538360464a085135f1ddf
proxymow <dev@proxymow.co.uk>
2026-06-21 10:02:10 +0100

    specific patterns for every saw dir-start permutation

---
+++ patterns/saw_HSE_PMPY.py
@@ -0,0 +1,5 @@
+from patterns import pmpy_saw
+
+
+def calculate_route(fence_points_pc, arena_width_m, arena_length_m, cutter_dia_m, logger, debug):
+    return pmpy_saw.calculate_route(fence_points_pc, arena_width_m, arena_length_m, cutter_dia_m, logger, debug, start_corner='SE', direction='H')
